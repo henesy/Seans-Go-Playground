428863b72381fac608ef8e3db6c4bb8392809f63
Sean Hinchee <henesy.dev@gmail.com>
2015-05-09 13:26:23 -0500

    adding fib.go, useless fibonacco PoS/PoC

---
+++ fib.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+   fibs := make([]float32, 100)
+   fibs[0]=0.0
+   fibs[1]=1.0
+   fmt.Printf("%3d: %.0f\n", 1, fibs[0])
+   fmt.Printf("%3d: %.0f\n", 2, fibs[1])
+   for i:=2;i<len(fibs);i+=1 {
+       nums := fibs[i-2:i]
+       fibs[i] = nums[0] + nums[1]
+       fmt.Printf("%3d: %.0f\n", i+1, fibs[i])
+   }
+}
