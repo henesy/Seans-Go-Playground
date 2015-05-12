0199682338e8898b9c31e5a524fe9b64206bb49f
Sean Hinchee <henesy.dev@gmail.com>
2015-05-12 17:14:35 -0500

    Adding updates to pointers and physics. Fixed bug in physics in which blanks weren't read.

--- physics.go
+++ physics.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "math"
 sc  "strconv"
+    "flag"
 )
 
 type Direction int
@@ -22,6 +23,7 @@ const (
 )
 
 type Physical struct {
+    i int /* placeholder for array */
     vi float64 /* initial vertical velocity (m/s)*/
     vf float64 /* final vertical velocity (m/s)*/
     a float64 /* vertical acceleration (m/s^2)*/
@@ -44,6 +46,7 @@ type Physical struct {
 
 /* counterpart to Physical that shows if we know a value or not (have solved for, etc.) */
 type Knowledge struct {
+    i int /* placeholder for array */
     vi Num /* initial vertical velocity (m/s)*/
     vf Num /* final vertical velocity (m/s)*/
     a Num /* vertical acceleration (m/s^2)*/
@@ -73,23 +76,30 @@ type Surface struct {
 /* Objects is the interface for a Physical */
 type Object interface {
     DEquals()
+    VFSquaredEquals()
+    TEquals()
 }
 
-/* */
+/* Functions to perform mathematical formulas */
 
 /* d=vi*t + (1/2)*a*t^2 */
-func (p *Physical) DEquals() {
+func (p *Physical) DEquals(knowns *[]Knowledge) {
     p.d = p.vi*p.t + 0.5*p.a*(math.Pow(p.t, 2))
+    /* the []index needs to read from the knowns pointer, thus reify the pointer
+    then write to index */
+    (*knowns)[p.i].d = KNOWN
 }
 
 /* vf^2 = vi^2 + 2*a*d */
-func (p *Physical) VFSquaredEquals() {
+func (p *Physical) VFSquaredEquals(knowns *[]Knowledge) {
     p.vf = math.Sqrt(math.Pow(p.vi, 2)+2*p.a*p.d)
+    (*knowns)[p.i].vf = KNOWN
 }
 
 /* t = (vf - vi)/a */
-func (p *Physical) TEquals() {
+func (p *Physical) TEquals(knowns *[]Knowledge) {
     p.t = (p.vf - p.vi)/p.a
+    (*knowns)[p.i].t = KNOWN
 }
 
 /* check for error, panic if found */
@@ -100,84 +110,92 @@ func check(err error) {
 }
 
 func main() {
-    fmt.Print("\nPlease fill out a number for each value prompted for. If unknown, type \"?\".\n")
-    fmt.Print("\nNumber of objects involved?: ")
-    readin := ""
-    fmt.Scanln(&readin)
-    num, err := sc.Atoi(readin)
-    check(err)
+    var num int
+    var err error
+    var specify bool
+    flag.IntVar(&num, "n", 1, "Number of objects to crunch for.")
+    flag.BoolVar(&specify, "s", false, "Whether or not to choose the number of objects.")
+    flag.Parse()
+    fmt.Print("\nIf a value is unknown, type \"?\".\n")
+    if readin:=""; specify == true {
+        fmt.Print("\nNumber of objects involved?: ")
+        fmt.Scanln(&readin)
+        num, err = sc.Atoi(readin)
+        check(err)
+    }
     fmt.Print(num, " objects.\n")
     objects := make([]Physical, num)
     knowns := make([]Knowledge, num)
 
     for i:=0;i<num;i+=1 {
         words:=""
-        var err error
         fmt.Print("\nObject ", i+1, ". \n")
+        objects[i].i, knowns[i].i = i, i
 
         fmt.Print("Initial Vertical Velocity (meters/second): ")
         fmt.Scanln(&words)
-        if words != "?" {
+        if words == "?" || words == "" || words == " " {
+            knowns[i].vi=UNKNOWN
+        } else if words != "?" {
             objects[i].vi, err = sc.ParseFloat(words, 64)
             knowns[i].vf=KNOWN
             check(err)
             knowns[i].vi=KNOWN
-        } else if words == "?" || words == "" || words == " " {
-            knowns[i].vi=UNKNOWN
         }
+
 
         fmt.Print("Final Vertical Velocity (m/s): ")
         fmt.Scanln(&words)
-        if words != "?" {
+        if words == "?" || words == "" || words == " " {
+            knowns[i].vf=UNKNOWN
+        } else if words != "?" {
             objects[i].vf, err = sc.ParseFloat(words, 64)
             check(err)
             knowns[i].vf=KNOWN
-        } else if words == "?" || words == "" || words == " " {
-            knowns[i].vf=UNKNOWN
         }
 
         fmt.Print("Acceleration (meters/second^2): ")
         fmt.Scanln(&words)
-        if words != "?" {
+        if words == "?" || words == "" || words == " " {
+            knowns[i].a=UNKNOWN
+        } else if words != "?" {
             objects[i].a, err = sc.ParseFloat(words, 64)
             check(err)
             knowns[i].a=KNOWN
-        } else if words == "?" || words == "" || words == " " {
-            knowns[i].a=UNKNOWN
         }
 
         fmt.Print("Displacement (meters): ")
         fmt.Scanln(&words)
-        if words != "?" {
+        if words == "?" || words == "" || words == " " {
+            knowns[i].d=UNKNOWN
+        } else if words != "?" {
             objects[i].d, err = sc.ParseFloat(words, 64)
             check(err)
             knowns[i].d=KNOWN
-        } else if words == "?" || words == "" || words == " " {
-            knowns[i].d=UNKNOWN
         }
 
         fmt.Print("Time (seconds): ")
         fmt.Scanln(&words)
-        if words != "?" {
+        if words == "?" || words == "" || words == " " {
+            knowns[i].t=UNKNOWN
+        } else if words != "?" {
             objects[i].t, err = sc.ParseFloat(words, 64)
             check(err)
             knowns[i].t=KNOWN
-        } else if words == "?" || words == "" || words == " " {
-            knowns[i].t=UNKNOWN
         }
 
         /* perform calculations to solve for a missing variable; must know at least 3 things */
         fmt.Print("\nResults: \n")
         if knowns[i].t == KNOWN && knowns[i].a == KNOWN && knowns[i].vi == KNOWN {
-            (&objects[i]).DEquals()
+            (&objects[i]).DEquals(&knowns)
             fmt.Print("'d' = ", objects[i].d, "\n")
         }
         if knowns[i].vi == KNOWN && knowns[i].a == KNOWN && knowns[i].d == KNOWN {
-            (&objects[i]).VFSquaredEquals()
+            (&objects[i]).VFSquaredEquals(&knowns)
             fmt.Print("'vf' = ", objects[i].vf, "\n")
         }
         if knowns[i].vi == KNOWN && knowns[i].vf == KNOWN && knowns[i].a == KNOWN {
-            (&objects[i]).TEquals()
+            (&objects[i]).TEquals(&knowns)
             fmt.Print("'t' = ", objects[i].t, "\n")
         }
 
