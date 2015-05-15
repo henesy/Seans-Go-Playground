f9a84e96d575469279495e42d9d6d310678b968c
Sean Hinchee <henesy.dev@gmail.com>
2015-05-15 12:06:02 -0500

    adding anonymous and closure functions

---
+++ anon1.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "fmt"
+sc  "strconv"
+)
+
+type maf func(...int)(int)
+
+func decide(nums ...int) maf {
+    tot := 0
+    for _, i := range nums {
+        tot += i
+    }
+
+    if tot < 100 {
+        return func(numz ...int)(int) {
+            sum := 0
+            for _, n := range numz {
+                sum += n
+            }
+            return sum
+        }
+    }
+
+    return func(numz ...int)(int) {
+        dif := 0
+        for _, n := range numz {
+            dif -= n
+        }
+        return dif
+    }
+}
+
+func check(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
+/* adds all integers */
+
+func main() {
+    words := ""
+    fmt.Print("How many integers?: ")
+    fmt.Scanln(&words)
+    nw, err := sc.Atoi(words)
+    check(err)
+    arr := make([]int, nw)
+
+    for pos, _ := range arr {
+        wrd := ""
+        fmt.Print("integer: ")
+        fmt.Scanln(&wrd)
+        n, err := sc.Atoi(wrd)
+        check(err)
+        arr[pos] = n
+    }
+
+    mathing := decide(arr...)
+    result := mathing(arr...)
+    fmt.Print(result, "\n")
+}
