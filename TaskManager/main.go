131c2adcc0fb36b7dbcea8d629e60739980588dc
Sean Hinchee <henesy.dev@gmail.com>
2015-05-17 01:52:37 -0500

    fixed crashes and weird bugs in client.go and TaskManager. Good times. Things should run better, if not smoothly.

--- TaskManager/main.go
+++ TaskManager/main.go
@@ -6,6 +6,22 @@ import (
     "time"
 )
 
+type state int
+const (
+    RUN state = iota
+    STOP
+)
+var toRun state = RUN
+
+
+func accepter(ln net.Listener, runChan chan state) {
+    for {
+        conn, err := ln.Accept()
+        check(err)
+        go handleConnection(conn, runChan)
+    }
+}
+
 /* runTime reports the time since startTime began */
 func runTime(startTime time.Time) {
     endTime := time.Since(startTime)
@@ -13,18 +29,25 @@ func runTime(startTime time.Time) {
 }
 
 /* handleConnection reads from data separately */
-func handleConnection(conn net.Conn) {
-    srvIn := make([]byte, 512)
+func handleConnection(conn net.Conn, runChan chan state) {
+    srvQuit := string([]byte("!srvquit"))
+
     addr := conn.RemoteAddr()
     fmt.Printf("'%v' connected.\n", addr)
     for {
+        srvIn := make([]byte, 512)
         n, err := conn.Read(srvIn)
         if err != nil {
             fmt.Printf("Connection '%v' suffered: '%v'", addr, err)
             break
         }
-        fmt.Printf("'%d' bytes; Data: '%s'; From: '%v'\n", n, string(srvIn), addr)
+        srvInString := string(srvIn)
+        fmt.Printf("'%d' bytes; Data: '%s'; From: '%v'\n", n, srvInString, addr)
         conn.Write([]byte("Received!\n"))
+        if srvInString[:len(srvQuit)] == srvQuit {
+            conn.Write([]byte("Closing connection!\n"))
+            runChan <- STOP
+        }
     }
 }
 
@@ -38,6 +61,8 @@ func check(err error) {
 /* Simple task manager written in Go, using concurrency; possibly networking */
 
 func main() {
+    runChan := make(chan state, 1)
+
     /* start a master timer to track how long taskmanager ran */
     startTime := time.Now()
     defer runTime(startTime)
@@ -46,9 +71,15 @@ func main() {
     ln, err := net.Listen("tcp", ":9090")
     check(err)
 
-    for {
-        conn, err := ln.Accept()
-        check(err)
-        go handleConnection(conn)
+    go accepter(ln, runChan)
+
+    for running := true; running == true; {
+        select {
+        case <- runChan:
+            close(runChan)
+            fmt.Println("STOPPING")
+            running = false
+        default:
+        }
     }
 }
--- client.go
+++ client.go
@@ -5,34 +5,53 @@ import (
     "net"
     "os"
     "bufio"
+    "io"
 )
 
-func readServer(conn net.Conn) {
-    words := make([]byte, 512)
-    for {
-        _, err := conn.Read(words)
-        check(err)
-        fmt.Print(string(words))
-    }
-}
-
-/* dialServer will connect to a pre-selected server */
-func dialServer(target string) {
-    //var words []byte
-    conn, err := net.Dial("tcp", target)
-    if err != nil {
-        fmt.Print(err, "\n")
-    }
-    /* get our info back from the server */
-    go readServer(conn)
-
+func readIn(conn net.Conn, runChan chan bool) {
     reader := bufio.NewReader(os.Stdin)
     for {
         words, _, _ := reader.ReadLine()
         _, err := conn.Write(words)
         check(err)
         if string(words) == "!quit" {
+            runChan <- false
             break
+        }
+    }
+}
+
+func readServer(conn net.Conn, runChan chan bool) {
+    words := make([]byte, 512)
+    for {
+        _, err := conn.Read(words)
+        if err == io.EOF {
+            fmt.Print("Disconnected from server.\n")
+            runChan <- false
+        } else {
+            check(err)
+        }
+        fmt.Print(string(words))
+    }
+}
+
+/* dialServer will connect to a pre-selected server */
+func dialServer(target string) {
+    runChan := make(chan bool, 1)
+    //var words []byte
+    conn, err := net.Dial("tcp", target)
+    if err != nil {
+        fmt.Print(err, "\n")
+    }
+    /* get our info back from the server */
+    go readServer(conn, runChan)
+    go readIn(conn, runChan)
+
+    for run := true; run == true; {
+        select {
+        case <- runChan:
+            run = false
+        default:
         }
     }
 }
