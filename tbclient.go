c1cbd02c5059a11facbd04e43a3cd0b812e3f388
Sean Hinchee <henesy.dev@gmail.com>
2015-05-18 09:02:17 -0500

    adding WIP termbox shclient

--- tbclient.go
+++ tbclient.go
@@ -4,9 +4,10 @@ import (
     "fmt"
     "github.com/nsf/termbox-go"
     "flag"
-    //"math/rand"
     //"net"
 )
+
+var msgs []string = make([]string, 50)
 
 /* prints to pos x, y */
 func tbPrint(x, y int, fg, bg termbox.Attribute, msg string) {
@@ -17,12 +18,28 @@ func tbPrint(x, y int, fg, bg termbox.Attribute, msg string) {
 }
 
 func draw(w, h int) {
+    defer termbox.Flush()
 
-        for y := 0; y < h; y++ {
-    		for x := 0; x < w; x++ {
-    			tbPrint(x, y, termbox.ColorBlue, termbox.ColorBlack, "*")
-    		}
-    	}
+    //top bar
+    tbPrint(0,0,termbox.ColorBlue, termbox.ColorBlack, "╔")
+    for y := 0; y < 1; y++ {
+        for x := 1; x < w-1; x++ {
+            tbPrint(x, y, termbox.ColorBlue, termbox.ColorBlack, "═")
+        }
+    }
+    tbPrint(w-1,0,termbox.ColorBlue, termbox.ColorBlack, "╗")
+
+    //body
+
+    //bottom bar
+    tbPrint(0,h-5,termbox.ColorBlue, termbox.ColorBlack, "╚")
+    for y := h-5; y < h-4; y++ {
+        for x := 1; x < w-1; x++ {
+            tbPrint(x, y, termbox.ColorBlue, termbox.ColorBlack, "═")
+        }
+    }
+    tbPrint(w-1,h-5,termbox.ColorBlue, termbox.ColorBlack, "╝")
+
 }
 
 /* client for SocketH rewritten in termbox-go */
@@ -52,8 +69,13 @@ func main() {
                 key := string(ev.Ch)
 
                     if key == "q" {
-                        pX++
-                        pY++
+                        if pX < w-1 && pY < h-1 {
+                            pX++
+                            pY++
+                        } else {
+                            pX--
+                            pY--
+                        }
                         tbPrint(pX, pY, termbox.ColorRed, termbox.ColorBlue, ":P")
                         termbox.Flush()
                     }
@@ -61,6 +83,7 @@ func main() {
                         run=false
                         termbox.Flush()
                     }
+
                     draw(w, h)
         }
     }
