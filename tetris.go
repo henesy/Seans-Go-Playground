4c58fa521cca1b51ff82d38c464eb946d3d63b1e
Sean Hinchee <henesy.dev@gmail.com>
2015-05-25 22:17:07 -0500

    adding play screen and support code

--- tetris.go
+++ tetris.go
@@ -8,7 +8,7 @@ import (
 
 var score, w, h int
 var running bool
-//var screen [][]occ
+//var screen [23][10]occ
 
 type dir int
 
@@ -31,19 +31,32 @@ type block struct {
     y int
 }
 
-type shape []block = make([]block, 9)
+type Shape [9]block
 
-type o shape
-type i shape
-type s shape
-type z shape
-type l shape
-type j shape
+type o Shape
+type i Shape
+type s Shape
+type z Shape
+type l Shape
+type j Shape
 
 type Shaper interface {
-
+	rotateLeft()
+	rotateRight()
+	moveLeft()
+	moveRight()
+	initShape()
 }
 
+
+/* sets all of screen to unoccupied */
+func unOccupy(screen [][]occ) {
+	for i := 0; i < len(screen); i++ {
+		for j := 0; j < len(screen[i]); j++ {
+			screen[i][j] = UN
+		}
+	}
+}
 
 /* prints to pos x, y */
 func tbPrint(x, y int, fg, bg termbox.Attribute, msg string) {
@@ -53,13 +66,54 @@ func tbPrint(x, y int, fg, bg termbox.Attribute, msg string) {
 	}
 }
 
-func draw(w, h int, drawChan chan dir) {
+func draw(w, h int, drawChan chan dir, screen [][]occ) {
 	for {
 		defer termbox.Flush()
 
 		termbox.Clear(termbox.ColorBlack, termbox.ColorBlack)
 
-		/* draw blocks @ current position */
+
+		/* draw box frames */
+		/* corners for screen */
+		tbPrint(0, 0, termbox.ColorWhite, termbox.ColorBlack, "╔")
+		tbPrint(0, len(screen)+1, termbox.ColorWhite, termbox.ColorBlack, "╚")
+		tbPrint(len(screen[0])+1, 0, termbox.ColorWhite, termbox.ColorBlack, "╗")
+		tbPrint(len(screen[0])+1, len(screen)+1, termbox.ColorWhite, termbox.ColorBlack, "╝")
+		/* bars for screen */
+		for y := 0; y < len(screen)+2; y++ {
+			for x := 0; x < len(screen[0])+2; x++ {
+				if (y == 0 || y == len(screen)+1) && (x != 0 && x != len(screen[0])+1) {
+					tbPrint(x, y, termbox.ColorWhite, termbox.ColorBlack, "═")
+				}
+				if (x == 0 || x == len(screen[0])+1) && (y != 0 && y != len(screen)+1) {
+					tbPrint(x, y, termbox.ColorWhite, termbox.ColorBlack, "║")
+				}
+			}
+		}
+
+
+
+		/* draw screen[][] */
+		x, y := 1, 1
+		for i := 0; i < len(screen); i++ {
+			for j := 0; j < len(screen[i]); j++ {
+				tbPrint(x, y, termbox.ColorBlue, termbox.ColorBlack, "*")
+				x++
+			}
+			y++
+			x = 1
+		}
+		/* draw occupancy in screen[][] */
+		//screen[0][0] = OC
+		//screen[len(screen)-1][len(screen[0])-1] = OC
+		for y := 0; y < len(screen); y++ {
+			for x := 0; x < len(screen[y]); x++ {
+				if screen[y][x] == OC {
+					tbPrint(x+1, y+1, termbox.ColorRed, termbox.ColorBlack, "█")
+				}
+			}
+		}
+
 
 		termbox.Flush()
         time.Sleep(60 * time.Millisecond)
@@ -83,11 +137,19 @@ func main() {
 	}
 	termbox.SetInputMode(termbox.InputAlt)
 	w, h = termbox.Size()
-    screen := [h][w]occ
+
+	//22 tall, 20 wide, top 2 will be hidden
+	var screen [][]occ = make([][]occ, 22)
+	for i := 0; i < len(screen); i++ {
+		screen[i] = make([]occ, 20)
+	}
+	unOccupy(screen)
+	//hiddenScreen := screen[0:2]
+
 
 	termbox.Clear(termbox.ColorBlack, termbox.ColorBlack)
 	termbox.Flush()
-	go draw(w, h, drawChan)
+	go draw(w, h, drawChan, screen)
 	termbox.Flush()
 	runChan <- S
 
