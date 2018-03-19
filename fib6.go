f5e38186ce06d7045baf70b22f707a6d7494a226
seh <seh@research-3.ece.iastate.edu>
2018-03-19 16:34:21 -0500

    fixed fib6 off by whatever

--- fib6.go
+++ fib6.go
@@ -12,8 +12,8 @@ func main() {
     var num int
     flag.IntVar(&num, "n", 10, "Set the number of fibonacci numbers to crunch [10]")
     flag.Parse()
-    f1, f2 := big.NewInt(0), big.NewInt(1)
-    for i:=1;i<num;i+=1 {
+    f1, f2 := big.NewInt(1), big.NewInt(1)
+    for i:=1;i<num+1;i+=1 {
        tmp := new(big.Int)
        tmp.Add(f1, f2)
        f1 , f2 = f2, tmp
