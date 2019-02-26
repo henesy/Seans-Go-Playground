b72ab93656dddb7377d17b5a0b21532c1d54d23e
seh <henesy.dev@gmail.com>
2019-02-25 21:12:16 -0600

    sarcast

---
+++ sarcast.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"io"
+	"unicode"
+)
+
+// Sarcastify text — WhAt dO YoU MeAn
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	U := true
+
+	for {
+		r, _, err := r.ReadRune()
+		if err != nil {
+			if err != io.EOF {
+				w.Write([]byte("err: " + err.Error()))
+			}
+			break
+		}
+		
+		if !(r == ' ' || r == '\t' || r == '\n') {
+			if U {
+				r = unicode.ToUpper(r)
+				U = false
+			} else {
+				r = unicode.ToLower(r)
+				U = true
+			}
+		}
+
+		w.WriteRune(r)
+	}
+	w.Flush()
+}
+
