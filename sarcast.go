d4950eb80b1d9e4c8bd74a9327f4a9436d25d25d
seh <henesy.dev@gmail.com>
2019-04-08 22:40:57 -0500

    add -t flag for making tiny text with sarcast

--- sarcast.go
+++ sarcast.go
@@ -5,14 +5,28 @@ import (
 	"os"
 	"io"
 	"unicode"
+	"flag"
+)
+
+var (
+	tiny	bool	// make tiny text y/n
 )
 
 // Sarcastify text — WhAt dO YoU MeAn
 func main() {
+	flag.BoolVar(&tiny, "t", false, "Make tiny text instead")
+	flag.Parse()
+
 	r := bufio.NewReader(os.Stdin)
 	w := bufio.NewWriter(os.Stdout)
-	U := true
 
+	if tiny {
+		tinify(r, w)
+		return
+	}
+
+	// Sarcastic text
+	U := true
 	for {
 		r, _, err := r.ReadRune()
 		if err != nil {
@@ -22,7 +36,7 @@ func main() {
 			break
 		}
 		
-		if !unicode.IsSpace(r) {
+		if !(unicode.IsSpace(r) || unicode.IsPunct(r)) {
 			if U {
 				r = unicode.ToUpper(r)
 				U = false
@@ -30,6 +44,36 @@ func main() {
 				r = unicode.ToLower(r)
 				U = true
 			}
+		}
+
+		w.WriteRune(r)
+	}
+	w.Flush()
+}
+
+// Make text tiny -- only replace [a-z0-9]
+func tinify(r *bufio.Reader, w *bufio.Writer) {
+	smol := []rune{'ᵃ','ᵇ','ᶜ','ᵈ','ᵉ','ᶠ','ᵍ','ʰ','ᶦ','ʲ','ᵏ','ˡ','ᵐ','ⁿ','ᵒ','ᵖ','ᵠ','ʳ','ˢ','ᵗ','ᵘ','ᵛ','ʷ','ˣ','ʸ','ᶻ'}
+	nums := []rune{'¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹','⁰'}
+
+	for {
+		r, _, err := r.ReadRune()
+		r = unicode.ToLower(r)
+		
+		if err != nil {
+			if err != io.EOF {
+				w.Write([]byte("err: " + err.Error()))
+			}
+			break
+		}
+
+		// Replace with small letter if able
+		if unicode.IsLetter(r) {
+			r = smol[r-'a']
+		}
+
+		if unicode.IsNumber(r) {
+			r = nums[r-'0']
 		}
 
 		w.WriteRune(r)
